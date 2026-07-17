2a828c2cef3f0a1d15917d2259fafeb2903b3e3a
Samarth Dubey <thesamarth62@gmail.com>
2026-07-18 02:09:39 +0530

    c

--- api/routers/scanning.py
+++ api/routers/scanning.py
@@ -9,7 +9,11 @@ from fastapi import APIRouter, Depends, HTTPException, Query, Request, Response,
 from fastapi.responses import StreamingResponse
 from pydantic import ValidationError, field_validator
 
-from api.auth import DEMO_SESSION_COOKIE, get_demo_session_key, verify_api_key, verify_api_key_or_session, verify_api_key_or_session_or_demo
+DEMO_SESSION_COOKIE = "guni_demo_id"
+
+def get_demo_session_key(request):
+    return "open", False
+    
 from api.input_validation import (
     StrictRequestModel,
     sanitize_choice,
